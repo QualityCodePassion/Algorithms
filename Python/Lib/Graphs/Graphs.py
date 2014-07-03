2e7fa682d1bd1051ba69be8e2303c8e3b0365b46
Tim Hale <hale_timo@hotmail.com>
2014-07-03 13:25:54 +1000

    Moved "Graphs" into its own module.

--- Python/Lib/Graphs/Graphs.py
+++ Python/Lib/Graphs/Graphs.py
@@ -10,6 +10,45 @@
 #-------------------------------------------------------------------------------
 
 
+# To import this module, you deed to add the following path to import the lib from a different directory
+#import sys
+#sys.path.insert(0, '../Lib/Graphs/')
+#import Graphs
+
+
+class Edge:
+    def __init__(self, y, w):
+        self.adjancentVertex = y
+        self.weight = w
+
+class EdgeList:
+    def __init__(self):
+        self.edges = []
+
+    def InsertEdge(self, y, w):
+        self.edges.append( Edge(y,w))
+
+class Graph(object):
+    def __init__(self, maxSize, directed = False):
+        print "init graph with max size of ", maxSize
+        self.directed = directed
+
+        # initialize the list (can't use the "*" opertor for this because it only does shallow
+        # copies pointing to the same "EdgeList" object.
+        self.vertices = []
+        for i in range(0, maxSize):
+            self.vertices.append(EdgeList())
+
+    def InsertEdge( self, x,y, directed = False, weight = 0 ):
+        vertex = self.vertices[x]
+        vertex.InsertEdge(y, weight)
+        if( not directed ):
+            self.InsertEdge( y, x, True, weight)
+
+# End of graph stuff
+###########################################################################################################
+
+
 def main():
     pass
 
--- Python/Stanford-Algo/Part-1/Prob-04-DFS-and-StroglyConnectedComponents/dfs.py
+++ Python/Stanford-Algo/Part-1/Prob-04-DFS-and-StroglyConnectedComponents/dfs.py
@@ -11,50 +11,17 @@
 
 import collections
 
+# To import this module, you deed to add the following path to import the lib from a different directory
+import sys
+sys.path.insert(0, '../../../Lib/Graphs/')
+import Graphs
+
+#import os, sys
+#lib_path = os.path.abspath('../../../Lib/Graphs/')
+#sys.path.append(lib_path)
+
 
 maxGraphSize = 100;
-
-
-###########################################################################################################
-# Todo uncooment the import below and put the graph staff in a module
-
-# Need to add the following path to import the lib from a different directory
-#import sys
-#sys.path.insert(0, '../Lib/Graphs/')
-#import Graphs
-
-
-class Edge:
-    def __init__(self, y, w):
-        self.adjancentVertex = y
-        self.weight = w
-
-class EdgeList:
-    def __init__(self):
-        self.edges = []
-
-    def InsertEdge(self, y, w):
-        self.edges.append( Edge(y,w))
-
-class Graph(object):
-    def __init__(self, maxSize, directed = False):
-        print "init graph with max size of ", maxSize
-        self.directed = directed
-
-        # initialize the list (can't use the "*" opertor for this because it only does shallow
-        # copies pointing to the same "EdgeList" object.
-        self.vertices = []
-        for i in range(0, maxSize):
-            self.vertices.append(EdgeList())
-
-    def InsertEdge( self, x,y, directed = False, weight = 0 ):
-        vertex = self.vertices[x]
-        vertex.InsertEdge(y, weight)
-        if( not directed ):
-            self.InsertEdge( y, x, True, weight)
-
-# End of graph stuff
-###########################################################################################################
 
 
 ###########################################################################################################
@@ -64,7 +31,7 @@ class Graph(object):
 def ReadGraphFromFile( fileName, maxSize ):
 
     print "Name of the file: ", fileName
-    graphFromFile = Graph(maxSize)
+    graphFromFile = Graphs.Graph(maxSize)
     for line in open(fileName):
         print line
         edge = line.split()
@@ -157,7 +124,7 @@ def main():
 
     BFS(graphFromFile, 1)
 
-    #testGraph = Graph(10)
+    #testGraph = Graphs.Graph(10)
     #testGraph.InsertEdge(1,7)
 
 if __name__ == '__main__':
