3103656a1bde65f11092234e519a7a89e85a183d
Tim Hale <hale_timo@hotmail.com>
2014-07-18 08:54:40 +1000

    Got the "Clustering.py" producing the correct answer, but the "hamming_distance.py" is still a work in progress. Might have to come back to this later.

--- Python/Stanford-Algo/Part-2/Prob-02-Clustering/Clustering.py
+++ Python/Stanford-Algo/Part-2/Prob-02-Clustering/Clustering.py
@@ -41,10 +41,10 @@ def ReadAndHeapifyWeightedEdges( fileName ):
 
 class set_union:
     def __init__(self, numberOfElements):
-        self.p = [0]*SET_SIZE; 		               # parent element
+        self.p = [-1]*SET_SIZE; 		               # parent element
         self.size = [1]*SET_SIZE;                  # number of elements in subtree i
     	self.n = numberOfElements;		           # number of elements in set
-        self.numberOfUnions = numberOfElements      # number of uninions in the set
+        self.numberOfUnions = numberOfElements      # number of unions in the set (i.e. at start only in union with themselves)
 
         for i in range(SET_SIZE):
             self.p[i] = i
@@ -52,31 +52,34 @@ class set_union:
 
     def find(self, x):
     	if (self.p[x] == x):
-    		return(x);
+    		return(x)
     	else:
-    		return( self.find(self.p[x]) );
+    		return( self.find(self.p[x]) )
 
 
     def union_sets(self, s1, s2):
-    	r1 = self.find(s1);
-    	r2 = self.find(s2);
+    	r1 = self.find(s1)
+    	r2 = self.find(s2)
 
     	if(r1 == r2):
-            return;		# already in same set
+            return		# already in same set
 
         # When I do a merge, decrease the "numberOfUnions"
         self.numberOfUnions -= 1
 
     	if (self.size[r1] >= self.size[r2]):
-    		self.size[r1] = self.size[r1] + self.size[r2];
-    		self.p[ r2 ] = r1;
+    		self.size[r1] = self.size[r1] + self.size[r2]
+    		self.p[ r2 ] = r1
     	else:
-            self.size[r2] = self.size[r1] + self.size[r2];
-            self.p[r1] = r2;
+            self.size[r2] = self.size[r1] + self.size[r2]
+            self.p[r1] = r2
 
 
     def same_component(self, s1, s2):
-    	return ( find(s1) == find(s2) );
+        p1 = self.find(s1)
+        p2 = self.find(s2)
+        same = ( p1 == p2 )
+    	return same
 
 
 
@@ -92,9 +95,15 @@ def clustering( numberOfNodes, heapEdges ):
         nextEdge = heappop( heapEdges)
         nodeUninion.union_sets(nextEdge[1], nextEdge[2])
         unionCount = nodeUninion.numberOfUnions
-        # The last edge pulled of the heap should be the maximum distance, because
-        # we are repeatedly pulling from a heap.
+
+    # The next edge pulled of the heap that isn't already in a clustere should be the maximum distance, because
+    # we are repeatedly pulling from a heap.
+    # Therefore, just pop off the head until I find an eddge that isn't already connected
+    same_cluster = True
+    while( same_cluster ):
+        nextEdge = heappop( heapEdges)
         maxDistance = nextEdge[0]
+        same_cluster = nodeUninion.same_component(nextEdge[1], nextEdge[2])
 
     return maxDistance
 
@@ -103,7 +112,7 @@ def clustering( numberOfNodes, heapEdges ):
 
 def main():
 
-    #heapEdges = ReadAndHeapifyWeightedEdges("test_clustering1.txt")
+    #numberOfNodes, heapEdges = ReadAndHeapifyWeightedEdges("test_clustering1.txt")
     numberOfNodes, heapEdges = ReadAndHeapifyWeightedEdges("clustering1.txt.bak")
     maxDistance = clustering(numberOfNodes, heapEdges)
     print "maxDistance = ", maxDistance
--- Python/Stanford-Algo/Part-2/Prob-02-Clustering/hamming_distance.py
+++ Python/Stanford-Algo/Part-2/Prob-02-Clustering/hamming_distance.py
@@ -10,17 +10,16 @@
 #-------------------------------------------------------------------------------
 
 #import gmpy
+from heapq import *
+
 
 def ReadBinaryValues( fileName ):
 
     print "Name of the file: ", fileName
     firstLine = True
+    debugCounter = 0
 
-    # Use a list of lists to populate the different groups in and make sure that
-    # the indices match the ones used for the list of nodes to do the comparisons
-    # with!
-    comparisonList = []
-    initialClusters = []
+    popcountHeap = []
 
     for line in open(fileName):
         if( firstLine ):
@@ -29,6 +28,7 @@ def ReadBinaryValues( fileName ):
             numberOfNodes = int(edge[0])
             firstLine = False
         else:
+            debugCounter += 1
             # TODO do I need to trim the whitespaces?
             #edge = line.split()
             # Convert the string into a binary value
@@ -39,39 +39,125 @@ def ReadBinaryValues( fileName ):
             binary = int(stripSpaces, 2)
             #print "binary = ", binary
 
-            if( not comparisonList ):
-                comparisonList.append(binary)
-                initList = [binary]
-                initialClusters.append(initList)
-            else:
-                fitsIntoCurrentCluster = False
-                for i in range(0, len(comparisonList)):
-                    compareTo = comparisonList[i]
-                    xor_result = compareTo ^ binary
-                    #TODO find a way to get gmpy working!
-                    #diff = gmpy.popcount(xor_result)
-                    diff = bin(xor_result).count("1")
-                    #print diff
-                    if( diff <= 2 ):
-                        initialClusters[i].append(binary)
-                        fitsIntoCurrentCluster = True
-                        break
+            #TODO find a way to get gmpy working!
+            #diff = gmpy.popcount(xor_result)
+            slow_popcount = stripSpaces.count("1")
+            popcountHeap.append( (slow_popcount, binary))
 
-                if( not fitsIntoCurrentCluster ):
-                    comparisonList.append(binary)
+    print "list size = ", len(popcountHeap)
+
+    heapify(popcountHeap)
+
+    return popcountHeap
+
+
+
+
+def ClusterPreFiltering( popcountHeap ):
+
+    print "Pre-Filtering size = ", len(popcountHeap)
+    firstLine = True
+
+    comparisonDictionary = {}
+    # Use a list of lists to populate the different groups in
+
+    # (Don't remember why I thought this was important): and make sure that
+    # the indices match the ones used for the list of nodes to do the comparisons
+    # with!
+    initialClusters = {}
+
+    sizeOfHeap = len(popcountHeap)
+    for index in range(0, sizeOfHeap):
+        nextNode = heappop(popcountHeap)
+        binary = nextNode[1]
+        targetPopcount = nextNode[0]
+
+        if( firstLine ):
+            firstLine = False
+            initCompareList = [binary]
+            comparisonDictionary[targetPopcount] = initCompareList
+            initClusterList = [binary]
+            initialClusters[targetPopcount] = initClusterList
+        else:
+            fitsIntoCurrentCluster = False
+
+
+
+
+
+
+            #use a different comparison list for each popcount, and only use the ones that are within a pop count of 2 of targetPopcount
+
+
+
+
+
+            for k in range(3):      #( max( (targetPopcount - 2), 0), min( (targetPopcount + 2), 24)):
+                if fitsIntoCurrentCluster:
+                    break
+
+                #start with same pop count then work backwards (can't work forwards because those popcounts haven't been pop off the heap yet)
+                if( k == 0):
+                    j = targetPopcount
+                elif( ( k == 1) and ( targetPopcount >= 1) ):
+                    j = targetPopcount - 1
+                elif( ( k == 2) and ( targetPopcount >= 2) ):
+                    j = targetPopcount - 2
+                else:
+                    break
+
+                if j in comparisonDictionary:
+                    comparisonList = comparisonDictionary[j]
+
+                    for i in range(0, len(comparisonList) ):
+                        compareTo = comparisonList[i]
+                        xor_result = compareTo ^ binary
+                        #TODO find a way to get gmpy working!
+                        #diff = gmpy.popcount(xor_result)
+                        diff = bin(xor_result).count("1")
+                        #print diff
+                        if( diff <= 2 ):
+
+this should be put onto the initialClusters for the corresponding binary value it matches up with, not for the targetPopcount.
+
+
+
+                            fitsIntoCurrentCluster = True
+                            if targetPopcount not in initialClusters:
+                                initClusterList = [binary]
+                                initialClusters[targetPopcount] = initClusterList
+                            else:
+                                #(initialClusters[targetPopcount]).append( (targetPopcount,initList) )
+                                add_list = (initialClusters[targetPopcount])
+                                add_list.append(binary)
+
+                            break
+
+            if( not fitsIntoCurrentCluster ):
+                if targetPopcount not in comparisonDictionary:
                     initList = [binary]
-                    initialClusters.append(initList)
+                    comparisonDictionary[targetPopcount] = initList
+                else:
+                    (comparisonDictionary[targetPopcount]).append(binary)
+
+                if targetPopcount not in initialClusters:
+                    initClusterList = [binary]
+                    initialClusters[targetPopcount] = initClusterList
+                else:
+                    (initialClusters[targetPopcount]).append(binary)
 
 
-
-    print "size = ", len(comparisonList)
+    print "size = ", len(comparisonDictionary)
 
     return initialClusters
 
 
+
 def main():
-    #initialClusters = ReadBinaryValues("test_binary_clustering.txt")
     initialClusters = ReadBinaryValues("binary_clustering_big.txt.bak")
+    #initialClusters = ReadBinaryValues("test_binary_clustering.txt")
+
+    ClusterPreFiltering(initialClusters)
 
 if __name__ == '__main__':
     main()
